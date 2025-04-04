fbd171e2235b3c81e1bc03a1ace0dc9ec56b9b9c
kylezchen <kchen25@priorypanther.com>
2025-04-04 14:32:24 -0700

    finalized the read method and now it works with the dictionary in there as well.

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 import java.util.Scanner;
 import javax.swing.*;
@@ -18,17 +19,12 @@ public class Main {
     public static void main(String[] args) {
         Dictionary dictionary = new Dictionary();
         try {
-            dictionary.read("dictionary.txt");
-            //will need to put dictionary file in here
+            dictionary.read("words.txt");
         } catch (FileNotFoundException e) {
             System.out.println("file not found");
         }
-
-        String searchWord = "hello";
-        //once we make a method that gets user input as a word, we can use this
-        if (dictionary.searchWord(searchWord)) {
-            //points += 400 or something
-            return;
+        if (dictionary.searchWord("hello")){
+            System.out.println("target found");
         }
         new GUI(500, 500);
     }
@@ -85,14 +81,18 @@ class GUI {
     }
 }
 class Dictionary {
-    ArrayList<String> words = new ArrayList<>();
+    public ArrayList<String> words;
+    public Dictionary(){
+        words = new ArrayList<>();
+    }
     public void read(String filename) throws FileNotFoundException {
         Scanner scanner = new Scanner(new File("src/" + filename));
         while (scanner.hasNextLine()){
-            String line = scanner.nextLine();
+            String line = scanner.nextLine().toLowerCase();
             words.add(line);
         }
         scanner.close();
+        Collections.sort(words);
     }
     public int binarySearchWord(String target, int lo, int hi) {
         if (lo > hi) {
@@ -109,6 +109,7 @@ class Dictionary {
         }
     }
     public boolean searchWord(String target) {
+        target = target.toLowerCase();
         return (binarySearchWord(target, 0, words.size() - 1) != -1);
     }
 }
